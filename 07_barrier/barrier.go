6a934c6ca8c1f20e003dab2e54e282a5de5a3d36
Maxim Altovskiy <keith@Maxims-MacBook-Air.local>
2026-09-21 00:07:13 +0200

    barrier

---
+++ 07_barrier/barrier.go
@@ -0,0 +1,36 @@
+package barrier
+
+import (
+	"homework1/internal/futex"
+	"sync/atomic"
+)
+
+type Barrier struct {
+	need    uint32
+	arrived uint32
+	round   uint32
+}
+
+func New(n int) *Barrier {
+	return &Barrier{
+		need: uint32(n),
+	}
+}
+
+func (b *Barrier) Wait() {
+	currentRound := atomic.LoadUint32(&b.round)
+
+	arrived := atomic.AddUint32(&b.arrived, 1)
+
+	if arrived == b.need {
+		atomic.StoreUint32(&b.arrived, 0)
+		atomic.AddUint32(&b.round, 1)
+
+		futex.WakeAll(&b.round)
+		return
+	}
+
+	for atomic.LoadUint32(&b.round) == currentRound {
+		futex.Wait(&b.round, currentRound)
+	}
+}
